4526d6e5a96b10caac6e0b55b4a729119b87d8e8
boazglick <95533522+boazglick@users.noreply.github.com>
2023-04-25 14:32:45 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "http://office3rock.com", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://office3rock.com", // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
